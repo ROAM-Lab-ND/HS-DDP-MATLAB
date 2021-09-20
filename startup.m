4f5d0135cb65fff526db6705fdae0776b31dbc8b
He Li <hli25@nd.edu>
2021-09-20 15:07:39 -0400

    minor edit

--- startup.m
+++ startup.m
@@ -1,1 +1,2 @@
-addpath(genpath ('/home/wensinglab/HL/Code/MHPC-project/MATLAB_v2'));
+% Add the project folder and its subfolders to MATALB search path
+addpath(genpath ('/home/wensinglab/HL/Code/HSDDP/MATLAB'));
