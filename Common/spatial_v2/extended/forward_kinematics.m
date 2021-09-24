fb02d0b340e0c16a097792b2ca1b851c266374ee
He Li <hli25@nd.edu>
2021-09-24 15:46:17 -0400

    minor change

--- Common/spatial_v2/extended/forward_kinematics.m
+++ Common/spatial_v2/extended/forward_kinematics.m
@@ -1,6 +1,7 @@
 function p = forward_kinematics(robot, q, linkidx, loc)
 % Compute forward kinamatics
-% Input q: generalized coordinate
+% Input robot: robot model built with spatial_v2
+%       q: generalized coordinate
 %       linkidx: link index
 %       loc: location of point of interest in local frame
 % return p: position of the point in base frame
@@ -8,8 +9,8 @@ q = q(:);
 loc = loc(:);
 T = eye(4);
 for i = 1:linkidx
-    [ XJ, ~ ] = jcalc( robot.model.jtype{i}, q(i) );
-    Xup{i} = XJ * robot.model.Xtree{i};
+    [ XJ, ~ ] = jcalc( robot.jtype{i}, q(i) );
+    Xup{i} = XJ * robot.Xtree{i};
     [R, r] = plux(Xup{i});
     R = R';
     T = T*[R, r; zeros(1,3), 1];
